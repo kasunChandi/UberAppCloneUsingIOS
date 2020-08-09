7d2017d87a10ec7e00c240a37387b72e702bc82f
Ruvinda Lahiru <ruvinda3@gmail.com>
2020-08-08 20:32:44 -0700

    Initial Commit

---
+++ RevitionIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RevitionIOS
+//
+//  Created by STUDENT on 8/8/20.
+//  Copyright © 2020 STUDENT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
